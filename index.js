1592a4bab5cbf0b954a11e3e020f0e6351efb457
mac <mac@macs-MacBook-Pro.local>
2023-08-04 13:31:00 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-Array.prototype.myReduce = function(callback, initialValue) {
-    let accumulator = initialValue;
-    for (let i = 0; i < this.length; i++) {
-      if (accumulator !== undefined) {
-        accumulator = callback(accumulator, this[i], i, this);
-      } else {
-        accumulator = this[i];
-      }
-    }
-    return accumulator;
-  };
-  
-  const numbers = [1, 2, 3, 4];
-  const sum = numbers.myReduce((total, currentNumber) => total + currentNumber);
-  console.log(sum); 
+// In readme
+
 
  
   
